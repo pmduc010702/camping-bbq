10f0784b63f1e042e0acc59c6c4189c692274437
pmduc010702 <pmduc010702@gmail.com>
2026-06-08 21:44:32 +0700

    fix vercel analytics

--- main.jsx
+++ main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ToastProvider } from './lib/toast.jsx';
+import { Analytics }from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ToastProvider>
       <App />
+      <Analytics />
     </ToastProvider>
   </React.StrictMode>
 );
